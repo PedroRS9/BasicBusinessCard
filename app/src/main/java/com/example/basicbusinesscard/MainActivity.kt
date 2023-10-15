0b3e79ccfa55afbd65ad34ec9991a9a8eb56fcc2
PedroRS9 <pedroglv98@gmail.com>
2023-10-15 20:42:08 +0100

    Full Name and Title created

--- app/src/main/java/com/example/basicbusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/basicbusinesscard/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.basicbusinesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.basicbusinesscard.ui.theme.BasicBusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BasicInfo(getString(R.string.name_text), getString(R.string.title_text))
                 }
             }
         }
@@ -30,17 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun BasicInfo(name: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ){
+        Text(
+            text = name,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = title,
+            fontSize = 8.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicBusinessCardTheme {
-        Greeting("Android")
+        BasicInfo(stringResource(R.string.name_text), stringResource(R.string.title_text))
     }
 }
